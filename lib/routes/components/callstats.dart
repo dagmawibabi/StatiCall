4a233d341da05cea5406dff954f0f887c16c81bf
Mussie Alemayehu <157012749+mussie-alemayehu@users.noreply.github.com>
2025-01-14 12:39:20 +0300

    extract the drop down to select sorting option

--- lib/routes/components/callstats.dart
+++ lib/routes/components/callstats.dart
@@ -1,5 +1,6 @@
 import 'package:callstats/widgets/all_calls_bar_graph.dart';
 import 'package:callstats/widgets/all_calls_pie_chart.dart';
+import 'package:callstats/widgets/dropdown_sortby_selector.dart';
 import 'package:flutter/material.dart';
 
 import 'package:callstats/routes/components/each_call_stat.dart';
@@ -159,134 +160,12 @@ class _CallStatsState extends State<CallStats> {
               ),
             // All Calls
             if (index == 0)
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10.0, bottom: 5.0),
-                        child: Icon(
-                          Icons.person_outline,
-                          color: Colors.grey[600],
-                        ),
-                      ),
-                      const SizedBox(width: 5.0),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 0.0, bottom: 5.0),
-                        child: Text(
-                          "All Calls",
-                          style: TextStyle(
-                            fontSize: 15.0,
-                            color: Colors.grey[700],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      right: 10.0,
-                      bottom: 5.0,
-                    ),
-                    child: DropdownButton(
-                      value: sortIndex,
-                      elevation: 0,
-                      icon: Padding(
-                        padding: const EdgeInsets.only(left: 5.0),
-                        child: Icon(
-                          Icons.sort,
-                          color: Colors.grey[700],
-                        ),
-                      ),
-                      isDense: true,
-                      style: TextStyle(
-                        fontSize: 14.0,
-                        color: Colors.grey[800],
-                      ),
-                      iconSize: 20.0,
-                      // itemHeight: 40.0,
-                      items: const [
-                        DropdownMenuItem(
-                          value: 0,
-                          child: Text(
-                            "All Calls",
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: 6,
-                          child: Text(
-                            "Missed Calls",
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: 7,
-                          child: Text(
-                            "Incoming Calls",
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: 8,
-                          child: Text(
-                            "Outgoing Calls",
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: 9,
-                          child: Text(
-                            "Rejected Calls",
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: 10,
-                          child: Text(
-                            "Blocked Calls",
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: 11,
-                          child: Text(
-                            "Unknown Calls",
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: 1,
-                          child: Text(
-                            "Total Duration",
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: 2,
-                          child: Text(
-                            "Max Duration",
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: 3,
-                          child: Text(
-                            "Min Duration",
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: 4,
-                          child: Text(
-                            "Oldest Date",
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: 5,
-                          child: Text(
-                            "Newest Date",
-                          ),
-                        )
-                      ],
-                      onChanged: (dynamic value) {
-                        sortIndex = value;
-                        widget.swapSort(value);
-                      },
-                    ),
-                  ),
-                ],
+              DropdownSortbySelector(
+                onChanged: (value) {
+                  sortIndex = value;
+                  widget.swapSort(value);
+                },
+                sortIndex: sortIndex,
               ),
             EachCallStatCard(
               curCall: widget.classifiedCallLogs[index],
