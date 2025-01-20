07a97e517c140ea8ec1d2521a2ad6887bd931749
Mussie Alemayehu <157012749+mussie-alemayehu@users.noreply.github.com>
2025-01-20 21:33:52 +0300

    add longest and shortest calls to the year wrapped model and extract them in the provider

--- lib/models/year_wrapped.dart
+++ lib/models/year_wrapped.dart
@@ -1,9 +1,15 @@
+import 'package:call_log/call_log.dart';
+
 class YearWrapped {
   final int totalCalls;
   final int totalCallDuration;
+  final CallLogEntry longestCall;
+  final CallLogEntry shortestCall;
 
   YearWrapped({
     required this.totalCalls,
     required this.totalCallDuration,
+    required this.longestCall,
+    required this.shortestCall,
   });
 }
--- lib/providers/year_wrapped_provider.dart
+++ lib/providers/year_wrapped_provider.dart
@@ -17,14 +17,28 @@ class YearWrappedProvider with ChangeNotifier {
 
     final totalCalls = callLog.length;
     int totalCallDuration = 0;
+    CallLogEntry longestCall = callLog[0], shortestCall = callLog[0];
 
     for (var callLogEntry in callLog) {
+      // add the call duration of the current call to the total duration
       totalCallDuration += callLogEntry.duration ?? 0;
+
+      // update the longest and shortest call variables if the call duration is not zero
+      if ((callLogEntry.duration ?? 0) != 0) {
+        if ((longestCall.duration ?? 0) < (callLogEntry.duration ?? 0)) {
+          longestCall = callLogEntry;
+        }
+        if ((shortestCall.duration ?? 0) > (callLogEntry.duration ?? 0)) {
+          shortestCall = callLogEntry;
+        }
+      }
     }
 
     _yearWrapped = YearWrapped(
       totalCalls: totalCalls,
       totalCallDuration: totalCallDuration,
+      longestCall: longestCall,
+      shortestCall: shortestCall,
     );
     notifyListeners();
   }
